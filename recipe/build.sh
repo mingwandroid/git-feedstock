347e35d09269e21150ae53fe785eefbe2a4d30d8
Ray Donnelly <mingw.android@gmail.com>
2017-07-08 12:36:10 +0100

    Add perl and tk
    This allows `git * -p`, something I struggle to live without and also
    `gitk`, something I've hardly ever used.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,34 +3,29 @@
 export C_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 
-if [ "$(uname)" == "Darwin" ]
-then
-    export LDFLAGS="-Wl,-rpath,$PREFIX/lib"
-elif [ "$(uname)" == "Linux" ]
-then
-    export LDFLAGS="-Wl,-rpath=$PREFIX/lib"
+if [[ $(uname) == Darwin ]]; then
+    export LDFLAGS="-Wl,-rpath,${PREFIX}/lib"
+elif [[ $(uname) == Linux ]]; then
+    export CFLAGS="-I${C_INCLUDE_PATH} "${CFLAGS}
+    export LDFLAGS="-Wl,-rpath=${LIBRARY_PATH} -L${LIBRARY_PATH} "${LDFLAGS}
 fi
 
-# NO_TCLTK disables git-gui
-# NO_PERL disables all perl-based utils:
-#   git-instaweb, gitweb, git-cvsserver, git-svn
-#   /ref http://www.spinics.net/lists/git/msg99803.html
 # NO_GETTEXT disables internationalization (localized message translations)
 # NO_INSTALL_HARDLINKS uses symlinks which makes the package 85MB slimmer (8MB instead of 93MB!)
 
 # Add a place for git config files.
-mkdir -p $PREFIX/etc
+mkdir -p "${PREFIX}/etc"
 make configure
 ./configure \
     --prefix="${PREFIX}" \
     --host=${HOST} \
     --with-gitattributes="${PREFIX}/etc/gitattributes" \
     --with-gitconfig="${PREFIX}/etc/gitconfig" \
-    --with-iconv="${PREFIX}/lib"
+    --with-iconv="${PREFIX}/lib" \
+    --with-perl="${PREFIX}/bin/perl" \
+    --with-tcltk="${PREFIX}/bin/tclsh"
 make \
-    --jobs="$CPU_COUNT" \
-    NO_TCLTK=1 \
-    NO_PERL=1 \
+    --jobs=${CPU_COUNT} \
     NO_GETTEXT=1 \
     NO_INSTALL_HARDLINKS=1 \
     all strip install
